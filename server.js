724ed8990d17d27d542458edb225a953d83feafb
om <omprakashnyn@gmail.com>
2026-02-13 21:39:47 +0530

    webhook fix

--- server.js
+++ server.js
@@ -62,6 +62,21 @@ app.post("/webhook", async (req, res) => {
   }
 
   res.sendStatus(200);
+
+});
+const VERIFY_TOKEN = "myverifytoken123";
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("Webhook verified");
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
 });
 
 
