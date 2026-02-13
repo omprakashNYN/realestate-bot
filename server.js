67d440644fbd1e2546a79530dd6f04f8b1402033
om <omprakashnyn@gmail.com>
2026-02-13 18:36:04 +0530

    first comment

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
